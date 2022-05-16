e524ad5fcc5e94ee9beb9196b10ab079ca1ca728
Musa <musa.uatbayev@gmail.com>
2022-05-16 19:25:28 +0600

    commit 3

--- resources/views/frontpage/application.blade.php
+++ resources/views/frontpage/application.blade.php
@@ -1,24 +1,24 @@
 @extends('front.frontp')
 @section('title')
-    Менің жазбаларым
+    Мои записи
 @endsection
 @section('content')
     <section class="py-5 bg-light">
         <div class="container px-5">
             <div class="row gx-5">
                 <div class="col-xl-12">
-                    <h2 class="fw-bolder fs-5 mb-4">Менің жазбаларым</h2>
+                    <h2 class="fw-bolder fs-5 mb-4">Мои записи</h2>
                     <!-- News item-->
                     <table class="table">
                         <thead>
                         <tr>
                             <th>#</th>
-                            <th>Тұрғын үй</th>
-                            <th>Бағасы</th>
-                            <th>Бөлме саны</th>
-                            <th>Жалпы ауданы</th>
-                            <th>Жалпы суммасы</th>
-                            <th>Статусы</th>
+                            <th>ЖК</th>
+                            <th>Цена</th>
+                            <th>Количество комнат</th>
+                            <th>Общая площадь</th>
+                            <th>Общая сумма</th>
+                            <th>Статус</th>
                         </tr>
                         </thead>
                         <tbody>
@@ -28,7 +28,7 @@
                                 <td>{{ $user_flat->rename }}</td>
                                 <td>{{ $user_flat->price }} т.</td>
                                 <td>{{ $user_flat->rname }}</td>
-                                <td>{{ $user_flat->totalarea }} ш.м</td>
+                                <td>{{ $user_flat->totalarea }} кв. м</td>
                                 <td>{{ ($user_flat->totalarea)*($user_flat->price) }} т.</td>
                                 <td><div class="badge bg-primary bg-gradient rounded-pill mb-2">{{ $user_flat->stname }}</div></td>
                                 <td></td>
